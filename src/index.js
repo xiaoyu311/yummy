93f5d9f23504305507996f4cdd97cfde09efc015
xiaoyu311 <616872074@qq.com>
2017-08-06 11:37:16 +0800

    finish

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import moment from 'moment';
-import 'moment/locale/zh-cn';
-moment.locale('zh-cn');
-
-ReactDOM.render(<App />, document.getElementById('root'));
--- src/settings.js
+++
@@ -1,5 +0,0 @@
-const Settings = {
-  host: 'http://yummy.haoduoshipin.com'
-}
-
-export default Settings
